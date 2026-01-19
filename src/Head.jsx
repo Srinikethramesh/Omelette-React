21a9405349f3c6d78aff9ee6a830286588227b64
Sriram Adithya <sriramadithya1999@gmail.com>
2026-01-19 11:05:39 +0530

    Fix image path

--- src/Head.jsx
+++ src/Head.jsx
@@ -4,7 +4,7 @@ import Preparation from "./Preparation"
 export default function Head(){
   return (
     <div className='card'>
-        <img src="public/image-omelette.jpeg" alt="" />
+        <img src="/image-omelette.jpeg" alt="" />
         <h1>Simple Omelette Recipe</h1>
         <p>  An easy and quick dish, perfect for any meal. This classic omelette combines beaten eggs cooked to perfection, optionally filled with your choice of cheese, vegetables, or meats.</p>
         <Preparation />
